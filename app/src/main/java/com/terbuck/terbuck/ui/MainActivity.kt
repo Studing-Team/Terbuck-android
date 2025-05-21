9f3beca86c3f899a807ae566dadb01fea00676c1
PIYUJIN <yjyj020201@gmail.com>
2025-05-22 03:36:25 +0900

    [Feat] #1  mainActivity 설정

--- app/src/main/java/com/terbuck/terbuck/ui/MainActivity.kt
+++ app/src/main/java/com/terbuck/terbuck/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.terbuck.terbuck
+package com.terbuck.terbuck.ui
 
 import android.content.pm.PackageManager
 import android.os.Build
@@ -10,6 +10,7 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.terbuck.terbuck.R
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
 
@@ -25,4 +26,20 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.P)
+    fun getKeyHash() {
+        try {
+            val info = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNING_CERTIFICATES)
+            for (signature in info.signingInfo?.apkContentsSigners!!) {
+                val md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                val keyHash = Base64.encodeToString(md.digest(), Base64.NO_WRAP)
+                Log.d("KeyHash", keyHash)  // 키 해시를 로그로 출력
+            }
+        } catch (e: NoSuchAlgorithmException) {
+            Log.e("KeyHash", "Unable to get MessageDigest. signature= $e")
+        } catch (e: Exception) {
+            Log.e("KeyHash", "Exception: $e")
+        }
+    }
 }
