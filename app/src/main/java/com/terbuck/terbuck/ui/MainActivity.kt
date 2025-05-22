7cffa51fed062679eb7ead4acc3f64387b1a3dcf
PIYUJIN <yjyj020201@gmail.com>
2025-05-22 17:06:13 +0900

    [Feat] #1  bottom navigation 구성

--- app/src/main/java/com/terbuck/terbuck/ui/MainActivity.kt
+++ app/src/main/java/com/terbuck/terbuck/ui/MainActivity.kt
@@ -11,19 +11,22 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.terbuck.terbuck.R
+import com.terbuck.terbuck.databinding.ActivityMainBinding
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.bottomNavBar.itemIconTintList = null
+
+        setContentView(binding.root)
     }
 
     @RequiresApi(Build.VERSION_CODES.P)
