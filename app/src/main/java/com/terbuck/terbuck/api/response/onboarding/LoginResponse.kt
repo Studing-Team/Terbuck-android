47698c541efc2979a94856478e9568ba32d24017
PIYUJIN <yjyj020201@gmail.com>
2025-06-27 02:43:13 +0900

    [Feat] #14  로그인 API 수정사항 반영

--- app/src/main/java/com/terbuck/terbuck/api/response/onboarding/LoginResponse.kt
+++ app/src/main/java/com/terbuck/terbuck/api/response/onboarding/LoginResponse.kt
@@ -2,6 +2,7 @@ package com.terbuck.terbuck.api.response.onboarding
 
 data class LoginResponse(
     val redirect: Boolean,
+    val id: Int,
     val accessToken: String?,
     val refreshToken: String?,
 )
